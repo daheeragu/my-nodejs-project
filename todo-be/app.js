e68f6efb577e3d9d842201871245a5c5ab1e0fe9
Dahee Park <dahee5654@gmail.com>
2024-10-16 19:00:04 +0900

    Update all changes

--- todo-be/app.js
+++ todo-be/app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
+require("dotenv").config();
 
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+const PORT = process.env.PORT || 5000;
+console.log("mongouri", MONGODB_URI_PROD);
+//CORS 미들웨어 먼저 설정
+app.use(cors());
 app.use(bodyParser.json());
+// 위의 미들웨어 설정후 라우터 설정
 app.use("/api", indexRouter);
 
-const mongoURL = `mongodb://localhost:27017/todo-demo`;
+const mongoURL = MONGODB_URI_PROD;
 mongoose
-  .connect(mongoURL, { useNewUrlParser: true })
+  .connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("mongoose connected");
   })
@@ -17,6 +25,6 @@ mongoose
     console.log("DB connection is failed", err);
   });
 
-app.listen(5000, () => {
-  console.log("server is working on 5000");
+app.listen(PORT, () => {
+  console.log(`server is working on ${PORT}`);
 });
